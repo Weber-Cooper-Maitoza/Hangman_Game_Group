0a335345b74d4fc565fb9e643ef529c48a3e8cb4
Cooper-Maitoza <coopermaitoza@mail.weber.edu>
2024-07-10 12:35:40 -0600

    set up the username page "/"

---
+++ frontend/src/components/username.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function UserName() {
+  return(
+    <div>
+      <p>TODO: Ask for username</p>
+      <p>TODO: navigate to game page</p>
+    </div>
+  );
+}
