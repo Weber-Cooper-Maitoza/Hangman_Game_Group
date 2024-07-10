75f828d90f188fa5f8355b08f20950ed90ecf4c0
Cooper-Maitoza <coopermaitoza@mail.weber.edu>
2024-07-10 13:48:04 -0600

    hangman page

---
+++ frontend/src/components/hangman.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
+
+export default function UserName() {
+  return(
+    <div>
+      <p>TODO: make hangman.</p>
+    </div>
+  );
+}
