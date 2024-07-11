47bfdd1355c69ba3db2db14836b52a2a476feca3
Coleton Watt <electricninja102@gmail.com>
2024-07-10 23:08:31 -0600

    Loads Words to use in hangman

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = process.env.ATLAS_URI;
+const { open } = require("node:fs/promises");
 
 let _db;
 
@@ -25,6 +26,11 @@ module.exports = {
 				console.log(
 					"Pinged your deployment. You successfully connected to MongoDB!"
 				);
+				_db = client.db("hangman");
+				if (!(await _db.collection("words").findOne())) {
+					loadWords()
+					console.log("Words Loading")
+				}
 			} finally {
 				// Ensures that the client will close when you finish/error
 				//console.log("Closing the client")
@@ -38,3 +44,19 @@ module.exports = {
 		return _db;
 	},
 };
+
+async function loadWords() {
+	const file = await open("./db/anagram_dictionary.txt");
+
+	for await (const line of file.readLines()) {
+		if (line.length > 5 && line.length < 10) {
+			let myobj = {
+				_id: line,
+				wordlength: line.length,
+			};
+			_db.collection("words").insertOne(myobj);
+		}
+	}
+
+	console.log("words loaded")
+}
